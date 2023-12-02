c9f8d892898878b6c5b4cc13bc91d13cbe285256
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2023-12-02 19:05:29 +0530

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,18 @@
+const a = "anurag"; /* not change */
+let b = 12;/* can be change */
+var c = "342";/* can be change */
+d = "re";  /* can chnage */ /* default var */ /* can be accessed from anywhere */
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+a="123";
+b="anurag";
+c="67";
+d="hey";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+ 
