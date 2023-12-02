c0546e262a01455a4b414b04274d8d90741d0175
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2023-12-02 19:23:56 +0530

    Add files via upload

---
+++ DataTypes.js
@@ -0,0 +1,18 @@
+"use strict";   //to implement the code according to the new version of the javascript we use "use strict before starting with our code"
+/* 
+String => "anurag"
+Number => 45;
+bigint => Number greater than 2^53;
+boolean => true or false;
+null  => no value and it is an object type
+undefied => when we declare the variable value is not defined/assigned;
+Symbol => unique;
+*/
+/* 
+type of is use to knw about the variable, whether it is string symbol or other
+
+
+*/
+console.log(typeof "anurag");    //string
+console.log(typeof 12);          //number
+console.log(typeof null);        //object
