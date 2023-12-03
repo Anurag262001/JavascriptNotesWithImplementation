742800641a01638f9788be10221fb4a5cee28eba
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2023-12-03 13:37:41 +0530

    Add files via upload

---
+++ StackHeap.js
@@ -0,0 +1,11 @@
+//stack memory -> primitive  
+//In stack we get the copy of the variable
+
+let nam1 = "anurag";
+let nam2 = nam1;
+nam2 = "anvi";
+console.log(nam1);
+console.log(nam2);
+
+//heap memory or refrence memeory -> heap
+//In heap we get the reference
