9b616cec4b424a94d10af0350ffa3c13be2d0522
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2023-12-04 00:07:14 +0530

    Add files via upload

---
+++ objdestructuring.js
@@ -0,0 +1,17 @@
+const obj = {
+    name:"anurag",
+    surname:"sharma",
+    name2:"anvi",
+    surname2:"singh"
+}
+
+ const {name,surname,name2,surname2}=obj;
+console.log(name,surname,name2,surname2); 
+
+const {name:n,surname:s,name2:n2,surname2:s2}=obj;
+console.log(n,s,n2,s2);
+
+
+
+
+
