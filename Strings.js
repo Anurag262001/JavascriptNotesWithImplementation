df389f8328c262b0544cab80165a04d1fe830be0
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2023-12-03 14:49:55 +0530

    Add files via upload

---
+++ Strings.js
@@ -0,0 +1,8 @@
+const name = "Anurag";
+const name2 = "anvi";
+console.log(name + name2);
+
+const n = "Anurag";
+const na = "anvi";
+console.log(`hello my name is ${n} and my friend name is ${na}`);
+
